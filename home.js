9e664dc0c6952aa66e5fdae2a9109856c360da4b
MacroCrystal <161733873+MacroCrystal@users.noreply.github.com>
2024-06-03 06:21:46 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,4 @@
+document.getElementById('poster').addEventListener('click', function() {
+            // Mengarahkan ke halaman HTML lain
+            window.location.href = 'event.html';
+        });
